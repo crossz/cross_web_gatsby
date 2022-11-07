4c10f3a3a4bb8d912b66d0e6d515ce35545812cb
ajay <ajay.wu@take2.health>
2022-11-07 23:20:34 +0700

    :test_tube: exclude update and health tip mdx

--- gatsby-node.js
+++ gatsby-node.js
@@ -115,7 +115,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   allMdxList?.forEach((mdx) => {
     let path = mdx.fields?.slug
     if (!path) return
-    if (mdx.parent.relativeDirectory === 'updates') return
+    if (mdx.parent.relativeDirectory === 'updates' || mdx.parent.relativeDirectory === 'health-tips') return
     let component = null,
       defer = false
 
