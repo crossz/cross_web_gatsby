d8e0fd16f0c3aa8172c6072883ee27a16ddd00e5
ajay <ajay.wu@take2.health>
2022-11-08 09:26:35 +0700

    :test_tube: exclude campaign-post-posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -115,6 +115,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   allMdxList?.forEach((mdx) => {
     let path = mdx.fields?.slug
     if (!path) return
+    if (mdx.parent.relativeDirectory === 'campaign-page-posts') return
     let component = null,
       defer = false
 
