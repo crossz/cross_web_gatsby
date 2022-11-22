a94335018ab5d1a66056c40b90373ffa4175db78
ajay <ajay.wu@take2.health>
2022-11-22 08:44:45 +0700

    :test_tube: reset images max width to 1800

--- scripts/optimize.js
+++ scripts/optimize.js
@@ -3,8 +3,8 @@ const glob = require(`glob`)
 const fs = require(`fs-extra`)
 
 const matches = glob.sync(`content/images/**/*.{PNG,png,jpg,jpeg}`)
-const MAX_WIDTH = 800
-const QUALITY = 90
+const MAX_WIDTH = 1800
+const QUALITY = 100
 Promise.all(
   matches.map(async (match) => {
     const stream = sharp(match)
