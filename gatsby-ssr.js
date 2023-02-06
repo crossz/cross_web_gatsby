4eefed16798ce5ec45f62100d751b7357ead96e0
crossz <ukcrossa@gmail.com>
2023-02-06 15:33:41 +0800

    :sparkles: global HEAD insert by using onRenderBody in gatsby-ssr.js.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,22 @@
+import React from 'react'
+const { languages } = require('./languages')
 import { wrapRootElement, wrapPageElement } from './wrapElement'
 
-export { wrapRootElement, wrapPageElement }
+// const headComponents = [
+//   <link rel="alternate" key="languages-zh-hk" href="http://example.com/zh-HK" hrefLang="zh-HK" />,
+//   <link rel="alternate" key="languages-zh-cn" href="http://example.com/zh-CN" hrefLang="zh-CN" />,
+//   <link rel="alternate"key="languages-en" href="http://example.com/en" hrefLang="en" />,
+// ];
+
+const headComponents = languages.map((locale) => {
+  const hrefstring = `http://example.com/${locale}`
+  return(
+    <link rel="alternate" href={hrefstring} hrefLang={locale} />
+  )
+})
+
+const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents(headComponents);
+};
+
+export { wrapRootElement, wrapPageElement, onRenderBody }
