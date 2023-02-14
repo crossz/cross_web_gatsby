48fa678ad35ffb68772d9a754d6fbed76a3b005e
crossz <ukcrossa@gmail.com>
2023-02-14 13:17:36 +0800

    :test_tube: all (content/+assets/) big images resized to 1024 width.

--- scripts/optimize.js
+++ scripts/optimize.js
@@ -2,9 +2,28 @@ const sharp = require(`sharp`)
 const glob = require(`glob`)
 const fs = require(`fs-extra`)
 
-const matches = glob.sync(`content/images/**/*.{PNG,png,jpg,jpeg}`)
-const MAX_WIDTH = 1800
+const MAX_WIDTH = 1024
 const QUALITY = 100
+
+let matches = glob.sync(`content/images/**/*.{PNG,png,jpg,jpeg}`)
+Promise.all(
+  matches.map(async (match) => {
+    const stream = sharp(match)
+    const info = await stream.metadata()
+
+    if (info.width < MAX_WIDTH) {
+      return
+    }
+
+    const optimizedName = match.replace(/(\..+)$/, (match, ext) => `-optimized${ext}`)
+
+    await stream.resize(MAX_WIDTH).jpeg({ quality: QUALITY }).toFile(optimizedName)
+
+    return fs.rename(optimizedName, match)
+  })
+)
+
+matches = glob.sync(`src/assets/images/**/*.{PNG,png,jpg,jpeg}`)
 Promise.all(
   matches.map(async (match) => {
     const stream = sharp(match)
